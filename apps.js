64560f739ab0ef1828c66cb3d51049a811e3c791
Jorge <93343250+jsuazof@users.noreply.github.com>
2022-02-02 15:25:48 -0300

    final
    feriados chile

--- apps.js
+++ apps.js
@@ -8,18 +8,19 @@ $(() => {
       success(data) {
       //console.log(data);
        feriadosChile.html("");
-        Object.keys(data).forEach((item) => {
-          console.log(data);
+       data.data.forEach((item) => {
+          //console.log(data.data);
           feriadosChile.append(`
           <thead>
               <tr>
-                <th scope="col">Fecha: ${data.data[0].date}</th>
-                <th scope="col">Feriado: ${data.data[0].title}</th>
-                <th scope="col">Ley: ${data.data[0].law_id}</th>
-                <th scope="col">${data.data[0].extra}</th>
+                <th scope="col">${item.date}</th>
+                <th scope="col">${item.title}</th>
+                <th scope="col">Ley: ${item.law_id}</th>
+                <th scope="col">${item.extra}</th>
               </tr>
           </thead>
           <tbody>
+          
           `);
         });
       },
